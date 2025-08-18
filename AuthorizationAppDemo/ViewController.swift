1de6abd4190885828a9f8c9d5709c2f3397a3410
Konstantin <kostya777i@gmail.com>
2025-08-18 15:45:29 +0500

    Initial Commit

---
+++ AuthorizationAppDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthorizationAppDemo
+//
+//  Created by Konstantin on 18.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
